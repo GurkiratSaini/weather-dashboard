e59d86a51d0d33098329c98819197da2c8b4a19f
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-03-26 12:36:33 -0400

    added cdn links, fetch link for openweather api

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var apiUrl = fetch("https://api.openweathermap.org/data/2.5/weather?q=Toronto&APPID=3e8aa64128a8b382a871af127be1e2d0");
+
+function getWeather(cityName){
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=3e8aa64128a8b382a871af127be1e2d0`;
+
+    fetch(apiUrl)
+    .then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    })
+}
+
+getWeather("Toronto");
