327d7b759b3202f29bc59d620d3920dbabdbfa52
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-03-29 12:56:12 -0400

    more work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,36 @@
-var apiUrl = fetch("https://api.openweathermap.org/data/2.5/weather?q=Toronto&APPID=3e8aa64128a8b382a871af127be1e2d0");
+var uvIndex = "";
 
-function getWeather(cityName){
-    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=3e8aa64128a8b382a871af127be1e2d0`;
+
+
+function getWeather(){
+    var cityName = $("#cityName").val().trim();
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&APPID=3e8aa64128a8b382a871af127be1e2d0&units=imperial`;
 
     fetch(apiUrl)
     .then(function(response) {
         response.json().then(function(data) {
             console.log(data);
+            var cityLat = data.city.coord.lat;
+            var cityLong = data.city.coord.lon;
+            var temperature = data.list[0].main.temp;
+            var humidity = data.list[0].main.humidity;
+            var windSpeed = data.list[0].wind.speed;
+
+            $("#cityNameHeader").text(cityName);
+            $("#cityTemp").text(temperature + "F");
+            $("#cityHumidity").text(humidity + "%");
+            $("#cityWindSpeed").text(windSpeed + "mph");
+            
+            // set uv index by doing another fetch here!!!!!
         });
     })
 }
 
-// getWeather("Toronto");
+function setWeather() {
+
+}
+
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+    getWeather();
+});
