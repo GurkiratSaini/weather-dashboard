7b133c03f156fbb1dcc5cca3a6f26d75cc434c04
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-03-29 14:51:21 -0400

    added forecast data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var uvIndex = "";
 
 
 function getWeather(cityName){
-    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&APPID=3e8aa64128a8b382a871af127be1e2d0&units=imperial`;
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&APPID=3e8aa64128a8b382a871af127be1e2d0&units=imperial&exclude=current,minutely,hourly`;
 
     fetch(apiUrl)
     .then(function(response) {
@@ -11,6 +11,8 @@ function getWeather(cityName){
             console.log(data);
             var cityLat = data.city.coord.lat;
             var cityLong = data.city.coord.lon;
+
+            /* old data, displays using apiUrl, onecallUrl shows better info
             var temperature = data.list[0].main.temp;
             var humidity = data.list[0].main.humidity;
             var windSpeed = data.list[0].wind.speed;
@@ -19,8 +21,36 @@ function getWeather(cityName){
             $("#cityTemp").text(temperature + "F");
             $("#cityHumidity").text(humidity + "%");
             $("#cityWindSpeed").text(windSpeed + "mph");
-            
-            // set uv index by doing another fetch here!!!!!
+            */
+
+            var onecallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLong}&exclude=minutely,hourly,alerts&appid=3e8aa64128a8b382a871af127be1e2d0&units=imperial`;
+
+            fetch(onecallUrl)
+            .then(function(response){
+                response.json().then(function(data){
+                    console.log(data);
+                    var currentTemp = data.current.temp;
+                    var currentHumidity = data.current.humidity;
+                    var currentWindSpeed = data.current.wind_speed;
+                    var currentDate = data.current.dt;
+                    var currentUVIndex = data.current.uvi;
+                    var currentWeatherIcon = data.current.weather[0].icon;
+                    $(`#cityNameHeader`).html(`<h4>${cityName} (${currentDate}) <img src="https://openweathermap.org/img/w/${currentWeatherIcon}.png" /></h4>`);
+                    $(`#cityTemp`).text(`${currentTemp}F`);
+                    $(`#cityHumidity`).text(`${currentHumidity}%`);
+                    $(`#cityWindSpeed`).text(`${currentWindSpeed}mph`);
+                    $(`#cityUV`).text(`${currentUVIndex}`);
+
+                    // 5-day forecast
+                    // forecastDate Img Temp Humidity
+                    for (let i = 1; i<=5; i++) {
+                        $(`#forecastDate${i}`).text(data.daily[i].dt);
+                        $(`#forecastImg${i}`).attr("src", `https://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png`);
+                        $(`#forecastTemp${i}`).text(data.daily[i].temp.day + "F");
+                        $(`#forecastHumidity${i}`).text(data.daily[i].humidity + "%");
+                    }
+                })
+            });
         });
     })
 }
